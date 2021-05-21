581268ea0ce8e79c1241b2d1dead008f87d84157
boyko1989 <hunwiss89@gmail.com>
2021-05-21 12:17:01 +0400

    Experiment: routing and layout

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php 
-
+$url = ltrim($_SERVER['REQUEST_URI'], '/');
 $routes = array(
 	array('url' => '#^$|^\?#', 'view' => 'main'),
    array('url' => '#^layout/#i', 'view' => 'layout'),
    array('url' => '#^linux/#i', 'view' => 'linux'),
-	array('url' => '#^lay/(.*)#', 'view' => 'lay')
+	// array('url' => '#^lay/(?P<lang>[a-z]+/)#', 'view' => 'lay'),
+	array('url' => '#^lay/[a-z]+/[a-z]+\.[a-z]+#', 'view' => 'lay')
 );
-
-$url = ltrim($_SERVER['REQUEST_URI'], '/');
 
 foreach ($routes as $route){
 	if( preg_match($route['url'], $url, $match)){
 		$view = $route['view'];
-		if (!empty($view)){
+		if (!empty($view) and ($view !== 'lay')){
 			include 'about/'.$view.'/index.php';
-		} 
+		} elseif ($view == 'lay') {
+			include $match[0];
+		}
 		break;
 	}
 }
