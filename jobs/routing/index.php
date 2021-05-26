09424c716ab7e156717a6559c42366315d5b87e4
boyko1989 <hunwiss89@gmail.com>
2021-05-26 21:32:09 +0400

    Работающая новая версия роутинга на SWITCH

--- jobs/routing/index.php
+++ jobs/routing/index.php
@@ -4,51 +4,24 @@
 
    switch ($path[0]) {
 
-      case ''; 
+      # about
+      case '': include '../about/main/index.php';
+         break;
       case 'layout'; 
       case 'linux'; 
       case 'makets';      
-         include 'about.php';
+         include '../about/'.$path[0].'/index.php';
          break;
-
+      # jobs
+      case 'db';
+      case 'messages';
+      case 'testing';
       case 'request';
-      case 'phpinfo';
-      case 'jobs';
-         include 'jobs.php';
+         include '../jobs/'.$path[0].'/index.php';
          break;
      
       default:
          include 'errors.php';
          break;
    }
-
-// $routes = array(
-// 	array('url' => '#^$|^\?#', 'view' => 'main'),
-//    array('url' => '#^layout/#i', 'view' => 'layout'),
-//    array('url' => '#^linux/#i', 'view' => 'linux'),
-//    array('url' => '#^makets/#i', 'view' => 'makets'),
-// 	array('url' => '#^request/#i', 'view' => 'request'),
-// 	array('url' => '#^phpinfo/#', 'view' => 'phpinfo')
-// 	/*array('url' => '#^jobs/(?P<dir_name>[a-z0-9-]+)/(?P<script_name>[a-z0-9\.-]+)#i', 'view' => 'jobs')*/
-// 	//array('url' => '#^lay/[a-z]+/[a-z]+\.[a-z]+#', 'view' => 'lay')
-// );
-
-// foreach ($routes as $route){
-// 	if( preg_match($route['url'], $url, $match)){
-// 		$view = $route['view'];
-// 		if (!empty($view)){
-// 			include '../about/'.$view.'/index.php';
-// 		} /*elseif ($view == "jobs") {
-// 			include '../jobs/messages/mail.php';
-// 		}*/
-// 		break;
-// 	}
-// }
-
-
-
-// if( empty($match) ){
-// 	include '../errors/404.php';
-// 	exit;
-// }
-// ?>
+ ?>
--- public/index.php
+++ public/index.php
@@ -1,21 +1,5 @@
 <?php
    error_reporting(0);
-/*
-   Всё заработает, когда поправишь php.ini:
-   https://www.cyberforum.ru/php-beginners/thread805214.html
-   Для этого ты сделал перенаправление на phpinfo -- чтобы узнать где этот файл
-*/
-
-   include '../var/path.php';
-   // echo $path = JOBS.'routing/routing.php<br>';
-   // echo $_SERVER['DOCUMENT_ROOT'];
-   
+   include '../var/path.php';   
    require JOBS.'routing/index.php';
-   //echo $url;
-
-   echo '<br>';
-   echo '<pre>';
-   print_r($path);
-   echo '</pre>';
-   // include '../jobs/routing/routing.php';
 ?>
