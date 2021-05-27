283c1ffe0305162a629a9bc553396c032f0e04eb
boyko1989 <hunwiss89@gmail.com>
2021-05-27 21:44:11 +0400

    Начал делать раздел макетов

--- about/makets/index.php
+++ about/makets/index.php
@@ -1,7 +1,33 @@
 <?php 
-   // echo 'Здесь будут макеты';
+   $_SESSION['_title'] = 'Готовые макеты'; 
    include 'html/head.php';
-   include 'html/header.php';
-   // include 'html/main_body.php';
-
+   //include 'html/header.php';
+   echo $path[0].'<br>';
+   // include ROOT_URL.'/html/head.php';
+   // include ROOT_URL.'/html/header.php';
+   
 ?>
+<body>
+<div class="wrapper">
+   <header class="header">
+      <?php include "html/header.php";?>
+   </header>
+   <main class="page">
+      <div class="_container">
+         <div class="content">
+            <h1>Готовые макеты.</h1>
+            <p>Здесь Вы сможете посмотреть качество моей работы как верстальщика и бесплатно скачать понравившиеся макеты для ваших сайтов.</p>
+         </div>
+      </div>
+   </main>
+   <footer class="footer">
+      <div class="footer__content _container">
+      </div>
+   </footer>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script> 
+   <script src="/public/lay/js/vendors.min.js"></script>
+   <script src="/public/lay/js/app.min.js"></script>
+</body>
+
+</html>
