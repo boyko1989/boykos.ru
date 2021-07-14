5b42807409494d795ac5eaf567f5c24904e80384
boyko1989 <hunwiss89@gmail.com>
2021-07-14 19:50:51 +0400

    Добавил favicon

--- html/makets/index.php
+++ html/makets/index.php
@@ -1,49 +1,46 @@
 <!DOCTYPE html>
 <html lang="ru">
- <head>
- 	<title>Верстальщик Павел Бойко</title>
- 	<meta charset="UTF-8">
- 	<meta name="format-detection" content="telephone=no">
- 	<link rel="stylesheet" href="/css/style.css">	
- 	<link rel="shortcut icon" href="favicon.ico">
- 	<!-- <meta name="robots" content="noindex, nofollow"> -->
- 	<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"> -->
- 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	 <style>
-	 	.center{
-			 text-align: center;
-		 }
-	 </style>
- </head>
+
+<head>
+   <title>Верстальщик Павел Бойко</title>
+   <meta charset="UTF-8">
+   <meta name="format-detection" content="telephone=no">
+   <link rel="stylesheet" href="/css/style.css">
+   <link rel="shortcut icon" href="favicon.ico">
+   <!-- <meta name="robots" content="noindex, nofollow"> -->
+   <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"> -->
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
 <body>
-<div class="wrapper">
-   <header class="header">
-      <?php include "../header.php";?>
-   </header>
-   <main class="page">
-      <div class="_container">
-         <div class="content">
-            <h1>Готовые макеты.</h1>
-            <p>Здесь Вы сможете посмотреть качество моей работы как верстальщика и бесплатно скачать понравившиеся макеты для ваших сайтов.</p>
-            <div class="list_tile">
-               <ul>
-                  <li>
-                     <a href="#"><img src="img/chrome_aLDfS25IvH.png" alt="lorem"></a>
-                  </li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-               </ul>
+   <div class="wrapper">
+      <header class="header">
+         <?php include "../header.php"; ?>
+      </header>
+      <main class="page">
+         <div class="_container">
+            <div class="content">
+               <h1>Готовые макеты.</h1>
+               <p>Здесь Вы сможете посмотреть качество моей работы как верстальщика и бесплатно скачать понравившиеся макеты для ваших сайтов.</p>
+               <div class="list_tile">
+                  <ul>
+                     <li>
+                        <a href="#"><img src="img/chrome_aLDfS25IvH.png" alt="lorem"></a>
+                     </li>
+                     <li></li>
+                     <li></li>
+                     <li></li>
+                  </ul>
+               </div>
             </div>
          </div>
-      </div>
-   </main>
-   <footer class="footer">
-      <div class="footer__content _container">
-      </div>
-   </footer>
-</div>
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script> 
+      </main>
+      <footer class="footer">
+         <div class="footer__content _container">
+         </div>
+      </footer>
+   </div>
+   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
    <script src="/js/vendors.min.js"></script>
    <script src="/js/app.min.js"></script>
 </body>
--- request.php
+++ request.php
@@ -1,9 +1,28 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
    <title>Форма обращения</title>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+   <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+   <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+   <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+   <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+   <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+   <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+   <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+   <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+   <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+   <link rel="icon" type="image/png" sizes="192x192" href="favicon/android-icon-192x192.png">
+   <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+   <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+   <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+   <link rel="manifest" href="favicon/manifest.json">
+   <meta name="msapplication-TileColor" content="#AB6000">
+   <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+   <meta name="theme-color" content="#AB6000">
 
    <!-- include libraries(jQuery, bootstrap) -->
    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
@@ -13,16 +32,21 @@
    <!-- include summernote css/js -->
    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
-   
+
    <link rel="stylesheet" href="css/style.min.css">
-   <style>h1{padding: 80px 0 0 0;}</style>
+   <style>
+      h1 {
+         padding: 80px 0 0 0;
+      }
+   </style>
    <link rel="shortcut icon" href="favicon.ico">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body>
    <div class="wrapper">
       <header class="header">
-         <?php include "html/header.php";?>
+         <?php include "html/header.php"; ?>
       </header>
       <main class="page">
          <div class="_container">
@@ -30,7 +54,7 @@
                <h1>Форма для связи</h1>
                <p>Отправьте мне описание Вашего проекта, я обдумаю детали и свяжусь с Вами. Отправляя мне Ваши данные,
                   Вы можете быть уверены, что они не попадут в спам-базы или мошенникам.</p>
-               <?php include "html/forms/request_form.php";?>
+               <?php include "html/forms/request_form.php"; ?>
             </div>
          </div>
       </main>
@@ -41,4 +65,5 @@
    <script src="js/vendors.min.js"></script>
    <script src="js/app.min.js"></script>
 </body>
+
 </html>
