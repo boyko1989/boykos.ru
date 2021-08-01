db1108fac05bd8a30a062c7f2574f281c6a0f21c
boyko1989 <hunwiss89@gmail.com>
2021-08-01 09:44:58 +0400

    Добавил Яндекс Метрику в head

--- html/head.php
+++ html/head.php
@@ -54,4 +54,28 @@
 	</script>
 	<meta name="yandex-verification" content="de1a8e3f50956457" />
 
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript">
+		(function(m, e, t, r, i, k, a) {
+			m[i] = m[i] || function() {
+				(m[i].a = m[i].a || []).push(arguments)
+			};
+			m[i].l = 1 * new Date();
+			k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+		})
+		(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+		ym(83515828, "init", {
+			clickmap: true,
+			trackLinks: true,
+			accurateTrackBounce: true,
+			webvisor: true,
+			ecommerce: "dataLayer"
+		});
+	</script>
+	<noscript>
+		<div><img src="https://mc.yandex.ru/watch/83515828" style="position:absolute; left:-9999px;" alt="" /></div>
+	</noscript>
+	<!-- /Yandex.Metrika counter -->
+
 </head>
