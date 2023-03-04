b99e3f9abc0667d4d9de4fdfc376a32a759675b6
boyko1989 <hunwiss89@gmail.com>
2023-03-04 18:55:15 +0400

    [0.0.1] Состряпал простенькое приветствие

--- admin/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-error_reporting(0);
-include '../html/head.php';
-include 'main_admin.php';
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-   error_reporting(0);
-   include 'html/head.php';
-   include 'html/main_body.php';
-?>
